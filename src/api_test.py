f4645f77778118a042c5a101768d4ef4ef6b08bb
anferneealviar99 <anferneealviar99@hotmail.com>
2023-10-25 23:35:12 +0800

    testing api for database

--- src/api_test.py
+++ src/api_test.py
@@ -10,11 +10,17 @@ try:
 except mokkari.exceptions.AuthenticationError as e:
     print("Invalid user name and password.")
 
-# Get all Marvel comics for the week of 2021-06-07
-asm = api.issues_list({"series_name": "The Amazing Spider-Man", "issue_number": "50", "year_began": "1963", "publisher_name": "marvel"})
+# # Get all Marvel comics for the week of 2021-06-07
+# asm = api.issues_list({"series_name": "The Amazing Spider-Man", "issue_number": "50", "year_began": "1963", "publisher_name": "marvel"})
 
-# Print the results
-for i in asm:
-    print(f"{i.id} {i.issue_name}")
+# # Print the results
+# for i in asm:
+#     print(f"{i.id} {i.issue_name}")
 
 
+batman = api.issue(34760)
+
+credits = batman.credits
+
+for credit in credits:
+    print(f'{credit.creator}, {credit.role[0].name}')
