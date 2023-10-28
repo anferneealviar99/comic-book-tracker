e5c732dc798d219ce6aa95126d7fae348cf701a4
= <=>
2023-10-28 15:44:30 +0800

    just figuring out how the api works

--- src/api_test.py
+++ src/api_test.py
@@ -10,17 +10,29 @@ try:
 except mokkari.exceptions.AuthenticationError as e:
     print("Invalid user name and password.")
 
-# # Get all Marvel comics for the week of 2021-06-07
-# asm = api.issues_list({"series_name": "The Amazing Spider-Man", "issue_number": "50", "year_began": "1963", "publisher_name": "marvel"})
+# Get all Marvel comics for the week of 2021-06-07
+asm = api.issues_list({"series_name": "The Amazing Spider-Man", "issue_number": "50", "year_began": "1963", "publisher_name": "marvel"})
 
-# # Print the results
-# for i in asm:
-#     print(f"{i.id} {i.issue_name}")
+# Print the results
+for i in asm:
+    print(f"{i.id} {i.issue_name}")
 
 
-batman = api.issue(34760)
+# batman = api.issue(34760)
 
-credits = batman.credits
+# credits = batman.credits
 
-for credit in credits:
-    print(f'{credit.creator}, {credit.role[0].name}')
+# for credit in credits:
+#     print(f'{credit.creator}, {credit.role[0].name}')
+
+batman = api.issues_list(
+    {
+        "series_name": "Batman",
+        "issue_number": "1",
+        "year_began": "2011",
+        "publisher_name": "DC Comics"
+    }
+)
+
+for issue in batman:
+    print(f"{issue.id}, {issue.issue_name}")
