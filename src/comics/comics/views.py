8c9d39a6356ac58dc8ebe1e6f70660504dc282e9
anferneealviar99 <anferneealviar99@hotmail.com>
2024-09-02 19:41:25 +0800

    added parse_comic_search_input and fetch_comic_details

--- src/comics/comics/views.py
+++ src/comics/comics/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ComicBook, UserComic, Role, Person, ComicRole
 import requests 
+import regex as re
 
 @login_required
 def user_comics(request):
@@ -48,30 +49,63 @@ def add_comic(request):
                 issue_details = results_list[0]
                 
                 comic_id = issue_details['id']
-                url = f'{settings.METRON_API_URL}/issue/{comic_id}'
                 
-                # return handle_comic_data(request, url)
+                comic = ComicBook.objects.get_or_create(
+                    comic_id=comic_id,
+                    defaults={
+                        'title': issue_details['issue'],
+                        'series': issue_details['series'],
+                        'year': issue_details['year'],
+                        'cover_image_url': issue_details['image'] 
+                    }
+                )
+                
+                return redirect('fetch_comic_details', comic_id=comic_id)
             
         except requests.exceptions.RequestException as e:
             return render(request, 'error.html', {'message': str(e)})
         
-# def handle_comic_data(request, url):
-#     try:
-#         response = requests.get(url,
-#                                 auth=(settings.METRON_API_USERNAME, settings.METRON_API_PASSWORD))
-#         response.raise_for_status()
-#         issue_data = response.json()
+@login_required
+def fetch_comic_details(request, comic_id):
+    try:
+        comic = ComicBook.objects.get(id=comic_id)
+        details_url = f"{settings.METRON_API_URL}/issue/{comic.comic_id}"
         
-#         if isinstance(issue_data, dict):
-#             comic, created = ComicBook.objects.get_or_create(
-#                 comic_id = comic_id,
-#                 defaults={
-#                     'title': issue_data['issue']
-#                 }
-#             )
-#     except requests.exceptions.RequestException as e:
-#         return render(request, 'error.html', {'message': str(e)})
+        response = requests.get(
+            details_url,
+            auth=(settings.METRON_API_USERNAME, settings.METRON_API_PASSWORD)
+        )
         
-def parse_comic_search_input(comic_search_input):
-    pass
+        response.raise_for_status()
+        issue_details = response.json()
+        
+        comic.publisher = issue_details['publisher']['name']
+        
+        process_comic_roles(issue_details, comic)
 
+        UserComic.objects.get_or_create(user=request.user, comic=comic)
+
+        return redirect('user_comics')
+
+    except ComicBook.DoesNotExist:
+        return render(request, 'error.html', {'message': 'Comic was not found.'})
+    
+    except requests.exceptions.RequestException as e:
+        return render(request, 'error.html', {'message': str(e)})
+    
+def parse_comic_search_input(comic_search_input):
+    """
+    Parse user input to extract series name, year and issue
+    Assumes input format is something e.g. "Batman (2016) #1" or Superman #1
+    Returns: (series, year, issue)
+    """
+    match = re.match(r"^(.*?)(?:\s\((\d{4})\))?\s?#(\d+)?$", comic_search_input.strip())
+
+    if match:
+        series = match.group(1).strip()
+        year = match.group(2) if match.group(2) else None 
+        issue = match.group(3).strip() if match.group(3) else None
+    else:
+        series, year, issue = comic_search_input.strip(), None, None 
+        
+    return series, year, issue
