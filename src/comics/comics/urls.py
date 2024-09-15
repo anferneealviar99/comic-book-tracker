50b597ef2caeb1a09dbc41747d9daccc79da38b4
anferneealviar99 <anferneealviar99@hotmail.com>
2024-09-15 17:04:31 +0800

    removed previous view for home

--- src/comics/comics/urls.py
+++ src/comics/comics/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name='home'),
+    path("", dashboard_view, name="home"),
     path('my-comics/', user_comics, name='user_comics'),
     path('add-comic/', add_comic, name='add_comic'),
     path('select-comic/', select_comic, name='select_comic'),
